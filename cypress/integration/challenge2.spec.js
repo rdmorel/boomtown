dbea6e7f8879c1770a2c572056c22a4a845bccad
rdmorel <rmorel@linq.com>
2021-06-21 11:35:26 -0700

    formatting + removed unnecessary function

--- cypress/integration/challenge2.spec.js
+++ cypress/integration/challenge2.spec.js
@@ -20,23 +20,16 @@ describe('Challenge 2', () => {
         });
     }
 
-    function getNumResults($response) {
-        // function to count the number of results
-        // in the body of the API response
-        let $numResults = $response.body.results.length;
-        return $numResults
-    }
-
     it('can access Boomtown API and filter by keywords', () => {
         // get all results
         getApi().then(($response) => {
             // saving number of results
-            let $allResults = getNumResults($response);
+            let $allResults = $response.body.results.length;
             // make search query
-            let $searchQuery = '?query=test'
+            let $searchQuery = '?query=test';
             getApi($searchQuery).then(($response) => {
                 // compare number of results
-                let $searchResults = getNumResults($response);
+                let $searchResults = $response.body.results.length;
                 expect($searchResults).to.be.lessThan($allResults);
             });
         });
@@ -45,19 +38,19 @@ describe('Challenge 2', () => {
 
     it('can paginate results from API', () => {
         // paginate results into pages of 10, get first page of results
-        let $pageQuery = '?start=0&limit=10'
+        let $pageQuery = '?start=0&limit=10';
         getApi($pageQuery).then(($response) => {
             // check that there are 10 results in the page
-            let $paginatedResults = getNumResults($response)
+            let $paginatedResults = $response.body.results.length;
             expect($paginatedResults).to.equal(10);
             // fails here, appears to be paginating with
             // incorrect number of results per page
         });
         // paginate results into pages of 5, get second page or results
-        let $pageQuery2 = '?start=1&limit=5'
+        let $pageQuery2 = '?start=1&limit=5';
         getApi($pageQuery2).then(($response) => {
             // check that there are 5 results in the second page
-            $paginatedResults = getNumResults($response)
+            $paginatedResults = $response.body.results.length;
             expect($paginatedResults).to.equal(10);
         });
     });
@@ -66,12 +59,12 @@ describe('Challenge 2', () => {
         // initial request
         getApi().then(($response) => {
             // saving number of results
-            let $allResults = getNumResults($response);
+            let $allResults = $response.body.results.length;
             // make same request 10 more times
             for(let i=0; i < 10; i++){
                 getApi().then(($response) => {
                     // saving number of results
-                    let $newResults = getNumResults($response);
+                    let $newResults = $response.body.results.length;
                     // check that API returns the same number of results
                     expect($newResults).to.equal($allResults);
                     // fails here - API returns either 40 or 41 results
