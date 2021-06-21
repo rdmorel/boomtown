fe7bc7272633e47a0e27e5aa7988fc6f3379234a
rdmorel <rmorel@linq.com>
2021-06-21 11:14:04 -0700

    comments

--- locustfile.py
+++ locustfile.py
@@ -4,6 +4,11 @@ class TestUser(HttpUser):
     host = 'https://api.preprod.goboomtown.com/sdk/v1/kb/search'
     headers = {"X-Boomtown-Integration": "MF49ER", "X-Boomtown-Key": "4dZ8mxjtkdLW3KLZJdaP5uHWtjk4eLrBDnLJB2hA6EUS"}
     wait_time = between(5, 9)
+
+    # Not making any assertions here, just hitting the API with different queries
+
+    # Different requests are arbitrarily weighted - "query" being most likely to
+    # run and "paginatedQuery" least likely to run
     
     @task(3)
     def basicGet(self):
